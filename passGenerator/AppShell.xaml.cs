d15e846b03b314ecff01d0d0c041204365dd3efc
Paweł <paweladamowicz2890@gamil.com>
2023-09-26 13:02:09 +0200

    Add project files.

---
+++ passGenerator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace passGenerator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ passGenerator/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿
+
+namespace passGenerator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        Random CharSign = new Random();
+        Random TypeSign = new Random();
+        private char[][] Znaki =
+        {
+            new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'},
+            new char[]{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'},
+            new char[]{'1','2','3','4','5','6','7','8','9'},
+            new char[]{'@','!','#','$','%','^','&','*','<','>','/','?','(',')'}
+        };
+        private void btnGenClicked(object sender, EventArgs e)
+        {
+            bool[] Types = new bool[4] { false, false, false, false };
+            string wynik = "";
+            if(chkMaleLitery.IsChecked)
+            {
+                Types[0] = true;
+            }
+            if (chkDuzeLitery.IsChecked)
+            {
+                Types[1] = true;
+            }
+            if (chkCyfry.IsChecked)
+            {
+                Types[2] = true;
+            }
+            if (chkZnakiSpec.IsChecked)
+            {
+                Types[3] = true;
+            }
+            if (!Types[0] && !Types[1] && !Types[2] && !Types[3] && DlugoscEnt.Text != "") { }
+            else
+            {
+                hasloLbl.Text = wynik;
+                int passLength;
+                int TypeRand;
+                int CharRand;
+                int.TryParse(DlugoscEnt.Text, out passLength);
+                for (int i = 0; i < passLength; i++)
+                {
+                    TypeRand = TypeSign.Next(0, 4);
+                    if (Types[TypeRand])
+                    {
+                        CharRand = CharSign.Next(0, Znaki[TypeRand].Length);
+                        wynik += Znaki[TypeRand][CharRand];
+                    }
+                    else
+                        i--;
+                }
+                hasloLbl.Text += wynik;
+                SemanticScreenReader.Announce(hasloLbl.Text);
+            }
+           
+            
+
+        }
+        
+    }
+}
